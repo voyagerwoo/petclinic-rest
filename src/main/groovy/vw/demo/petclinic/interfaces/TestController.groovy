b82f6c1a3cca86f1e2a56c3accabb013461c55f0
voyagerwoo <voyager.woo@gmail.com>
2018-07-19 16:39:15 +0900

    Add stress more

--- src/main/groovy/vw/demo/petclinic/interfaces/TestController.groovy
+++ src/main/groovy/vw/demo/petclinic/interfaces/TestController.groovy
@@ -15,7 +15,7 @@ class TestController {
         log.info("stress check")
 
         Double sum = 0d
-        for (int i = 0; i < 1000000; i++) {
+        for (int i = 0; i < 10000000; i++) {
             sum += Math.sqrt(i)
         }
 
