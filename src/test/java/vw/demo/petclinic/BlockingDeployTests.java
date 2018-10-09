c46661040d350015a93cc3058ce0e8a35b375aa5
voyagerwoo <voyager.woo@gmail.com>
2018-10-09 19:52:53 +0900

    Add blocking deploy test

---
+++ src/test/java/vw/demo/petclinic/BlockingDeployTests.java
@@ -0,0 +1,12 @@
+package vw.demo.petclinic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class BlockingDeployTests {
+    @Test
+    public void blockDeploy() {
+        fail();
+    }
+}
