b37713705691a3f2e3cbb29852040b28da1aacda
voyagerwoo <voyager.woo@gmail.com>
2018-10-10 22:30:13 +0900

    Enable Scheduling

--- src/main/java/vw/demo/petclinic/PetclinicApplication.java
+++ src/main/java/vw/demo/petclinic/PetclinicApplication.java
@@ -2,8 +2,10 @@ package vw.demo.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PetclinicApplication {
 
     public static void main(String[] args) {
