e9d85185beba32509c3fe7cdaacade0b9dd9cc4f
voyagerwoo <voyager.woo@gmail.com>
2018-10-12 01:44:55 +0900

    Update stress test concurrently

--- src/main/java/vw/demo/petclinic/interfaces/tests/StressTestController.java
+++ src/main/java/vw/demo/petclinic/interfaces/tests/StressTestController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RestController
 @RequestMapping("/test/stress")
@@ -15,16 +17,23 @@ public class StressTestController {
     @GetMapping("")
     public ResponseEntity stress() {
         log.info("stress check");
+        ExecutorService es = Executors.newFixedThreadPool(20);
+        for (int i = 0; i < 20; i++) {
+            es.submit(this::calculateSqrt);
+        }
+        return ResponseEntity.ok(new HashMap<String, Object>() {{
+            put("result", "OK");
+            put("sum", calculateSqrt());
+        }});
+    }
 
+    private Double calculateSqrt() {
         Double sum = 0d;
         for (int i = 0; i < 10000000; i++) {
             sum += Math.sqrt(i);
         }
+        log.info("Calculate Result : " + sum);
 
-        Double finalSum = sum;
-        return ResponseEntity.ok(new HashMap<String, Object>() {{
-            put("result", "OK");
-            put("sum", finalSum);
-        }});
+        return sum;
     }
 }
